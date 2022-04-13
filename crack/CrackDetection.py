01451b7938fd752ff23ad4e17c2a83108ddd0b19
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2022-04-13 10:19:32 +0800

    setup

---
+++ crack/CrackDetection.py
@@ -0,0 +1,52 @@
+# importing necessary libraries
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+# read a cracked sample image
+img = cv2.imread('crack.bmp')
+
+# Convert into gray scale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Image processing ( smoothing )
+# Averaging
+blur = cv2.blur(gray,(3,3))
+
+# Apply logarithmic transform
+img_log = (np.log(blur+1)/(np.log(1+np.max(blur))))*255
+
+# Specify the data type
+img_log = np.array(img_log,dtype=np.uint8)
+
+# Image smoothing: bilateral filter
+bilateral = cv2.bilateralFilter(img_log, 5, 75, 75)
+
+# Canny Edge Detection
+edges = cv2.Canny(bilateral,100,200)
+
+# Morphological Closing Operator
+kernel = np.ones((5,5),np.uint8)
+closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+# Create feature detecting method
+# sift = cv2.xfeatures2d.SIFT_create()
+# surf = cv2.xfeatures2d.SURF_create()
+orb = cv2.ORB_create(nfeatures=1500)
+
+# Make featured Image
+keypoints, descriptors = orb.detectAndCompute(closing, None)
+featuredImg = cv2.drawKeypoints(closing, keypoints, None)
+
+cv2.imshow('Original', img)
+cv2.imshow('Output', featuredImg)
+cv2.waitKey()
+
+# Create an output image
+cv2.imwrite('CrackDetected-7.jpg', featuredImg)
+# Use plot to show original and output image
+plt.subplot(211),plt.imshow(img)
+plt.title('Original'),plt.xticks([]), plt.yticks([])
+plt.subplot(212),plt.imshow(featuredImg,cmap='gray')
+plt.title('Output Image'),plt.xticks([]), plt.yticks([])
+plt.show()
