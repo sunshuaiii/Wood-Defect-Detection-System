8976e1068fbd749e3c206d7df79f8a4ec22f746b
UtardaBest <alvinzzz@hotmail.com>
2022-04-16 11:08:43 +0800

    created a function for knot and include 3 knot pic to be tested in main

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import cv2
 import numpy
+from importlib.machinery import SourceFileLoader
+mymodule = SourceFileLoader('knotDetection', 'C:/Users/Alvin/.spyder-py3/Wood-Defect-Detection-System/knot/knotDetection.py').load_module()
+
+#im = cv2.imread('Untitled1.bmp')
+frame = cv2.imread('knot1.bmp')
+frame2 = cv2.imread('knot2.bmp')
+frame3 = cv2.imread('knot3.bmp')
+mymodule.knotDetection(frame)
+mymodule.knotDetection(frame2)
+mymodule.knotDetection(frame3)
