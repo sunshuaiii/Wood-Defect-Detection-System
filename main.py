2f7436ece0791439d1ea237d5e9a6504b48723ac
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2022-04-22 10:01:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def small_knot(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     lower_red = np.array([0, 0, 0])
-    upper_red = np.array([10, 255, 100])
+    upper_red = np.array([70, 255, 100])
 
     mask = cv2.inRange(hsv, lower_red, upper_red)
 
@@ -70,7 +70,7 @@ def small_knot(frame):
 
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
-        if cv2.contourArea(contour) < 800:
+        if cv2.contourArea(contour) < 800 or cv2.contourArea(contour) > 3000:
             continue
         knot_number = knot_number + 1
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -186,7 +186,7 @@ def pinhole(frame):
 
 
 def wood_defect_detection_system():
-    image_path = 'imageInput/crack/2.bmp'
+    image_path = 'imageInput/knot/1.bmp'
     frame = cv2.imread(image_path)
     print("Reading image from " + image_path)
 
